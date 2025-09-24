e9a9e0d03655c093e22421e5748c3950f140255a
MTDau <mdau@student.mtsac.edu>
2025-09-23 18:49:51 -0700

    Some logic update

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,5 +1,6 @@
 #include "tictactoe.h"
 #include "ui_tictactoe.h"
+#include <QMessageBox>
 
 TicTacToe::TicTacToe(QWidget *parent)
     : QMainWindow(parent)
@@ -24,6 +25,9 @@ TicTacToe::TicTacToe(QWidget *parent)
                     [=]() { handle_clicks(i, j); });
         }
     }
+
+    //Hide the show_winner
+    ui->show_winner->setVisible(false);
 }
 
 TicTacToe::~TicTacToe()
@@ -47,10 +51,22 @@ void TicTacToe::updateGame()
 void TicTacToe::handle_clicks(int row, int col) {//What to do after clicks
     func->put(row, col);
     updateGame();  // redraw board after every move
+    // Check for winner
+    char winner;
+    if (func->check_win(winner)) {
+        ui->show_winner->setVisible(true);  // show the frame
+        QMessageBox::information(this, "Game Over",
+            QString("Player %1 wins!").arg(QChar(winner)));
+    }
+    // Optional: check for draw
+    else if (func->get_turn() >= SIZE * SIZE) {
+        QMessageBox::information(this, "Game Over", "It's a draw!");
+    }
 }
 
 void TicTacToe::on_Reset_clicked()
 {
     func->reset();
     updateGame();
+    ui->show_winner->setVisible(false); // hide winner frame
 }
